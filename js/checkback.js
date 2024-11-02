4e80036c4cd5411e680278495deb42030271e415
Forwaken <cbower7534@gmail.com>
2024-11-02 18:12:11 -0400

    Maybe fixes bug in new functions, we will see?

--- js/checkback.js
+++ js/checkback.js
@@ -654,27 +654,19 @@
         player.cb.canteEnergyXPBoostButtonBase = [new Decimal(10), new Decimal(30)]
         player.cb.canteEnergyPetPointButtonBase = [new Decimal(0.12), new Decimal(0.05), new Decimal(0.8), new Decimal(7), new Decimal(0.3),new Decimal(1),new Decimal(0.002),]
     },
-    levelToXP(lev)
+    levelToXP(l)
     {
-        let xxpp = 0
-        if (lev == 0) {
-            return 0
-        } else {
-            xxpp = ((lev.add(3)).pow(2.2)).mul(5/11)
-            xxpp = xxpp.div(player.cb.uncommonPetEffects[2][2])
-            xxpp = xxpp.div(player.cb.rarePetEffects[3][1])
-            return xxpp
-        }
+        let xxpp = new Decimal(0)
+        xxpp = ((l.add(3)).pow(2.2)).mul(5/11)
+        xxpp = xxpp.div(player.cb.uncommonPetEffects[2][2])
+        xxpp = xxpp.div(player.cb.rarePetEffects[3][1])
+        return xxpp
     },
-    xpToLevel(xxpp)
+    xpToLevel(x)
     {
-        xxpp = xxpp.mul(player.cb.uncommonPetEffects[2][2])
-        xxpp = xxpp.mul(player.cb.rarePetEffects[3][1])
-        if (xxpp < 5) {
-            return 0
-        } else {
-            return ((xxpp.div(5/11)).pow(5/11)).sub(3).floor()
-        }
+        x = x.mul(player.cb.uncommonPetEffects[2][2])
+        x = x.mul(player.cb.rarePetEffects[3][1])
+        return ((x.div(5/11)).pow(5/11)).sub(3).floor()
     },
     levelup()
     {
