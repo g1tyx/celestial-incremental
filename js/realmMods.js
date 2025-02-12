7917e4516790dd36ff730b283b060458a1cadf96
Icecreamdude <72051053+Icecreamdudes@users.noreply.github.com>
2025-02-12 01:31:07 -0500

    s

--- js/realmMods.js
+++ js/realmMods.js
@@ -818,7 +818,7 @@
                     setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
                 } else
                 {
-                    let max = Decimal.affordGeometricSeries(player.rm.realmEnergy[0], this.costBase(), this.costGrowth(), getBuyableAmount(this.layer, this.id))
+                    let max = Decimal.affordGeometricSeries(player.rm.realmEnergy[0], base, growth, getBuyableAmount(this.layer, this.id))
                     if (max.gt(this.purchaseLimit().sub(getBuyableAmount(this.layer, this.id)))) { max = this.purchaseLimit().sub(getBuyableAmount(this.layer, this.id)) }
                 let cost = Decimal.sumGeometricSeries(max, base, growth, getBuyableAmount(this.layer, this.id))
                 if (!hasMilestone("s", 16)) player.rm.realmEnergy[0] = player.rm.realmEnergy[0].sub(cost)
@@ -852,7 +852,7 @@
                 } else
                 {
 
-                    let max = Decimal.affordGeometricSeries(player.rm.realmEnergy[0], this.costBase(), this.costGrowth(), getBuyableAmount(this.layer, this.id))
+                    let max = Decimal.affordGeometricSeries(player.rm.realmEnergy[0], base, growth, getBuyableAmount(this.layer, this.id))
                     if (max.gt(this.purchaseLimit().sub(getBuyableAmount(this.layer, this.id)))) { max = this.purchaseLimit().sub(getBuyableAmount(this.layer, this.id)) }
                 let cost = Decimal.sumGeometricSeries(max, base, growth, getBuyableAmount(this.layer, this.id))
                 if (!hasMilestone("s", 16)) player.rm.realmEnergy[0] = player.rm.realmEnergy[0].sub(cost)
@@ -886,7 +886,7 @@
                 } else
                 {
 
-                    let max = Decimal.affordGeometricSeries(player.rm.realmEnergy[1], this.costBase(), this.costGrowth(), getBuyableAmount(this.layer, this.id))
+                    let max = Decimal.affordGeometricSeries(player.rm.realmEnergy[1], base, growth, getBuyableAmount(this.layer, this.id))
                     if (max.gt(this.purchaseLimit().sub(getBuyableAmount(this.layer, this.id)))) { max = this.purchaseLimit().sub(getBuyableAmount(this.layer, this.id)) }
                 let cost = Decimal.sumGeometricSeries(max, base, growth, getBuyableAmount(this.layer, this.id))
                 if (!hasMilestone("s", 16)) player.rm.realmEnergy[1] = player.rm.realmEnergy[1].sub(cost)
@@ -919,7 +919,7 @@
                     setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
                 } else
                 {
-                    let max = Decimal.affordGeometricSeries(player.rm.realmEnergy[1], this.costBase(), this.costGrowth(), getBuyableAmount(this.layer, this.id))
+                    let max = Decimal.affordGeometricSeries(player.rm.realmEnergy[1], base, growth, getBuyableAmount(this.layer, this.id))
                     if (max.gt(this.purchaseLimit().sub(getBuyableAmount(this.layer, this.id)))) { max = this.purchaseLimit().sub(getBuyableAmount(this.layer, this.id)) }
                 let cost = Decimal.sumGeometricSeries(max, base, growth, getBuyableAmount(this.layer, this.id))
                 if (!hasMilestone("s", 16)) player.rm.realmEnergy[1] = player.rm.realmEnergy[1].sub(cost)
@@ -953,7 +953,7 @@
                 } else
                 {
 
-                    let max = Decimal.affordGeometricSeries(player.rm.realmEnergy[2], this.costBase(), this.costGrowth(), getBuyableAmount(this.layer, this.id))
+                    let max = Decimal.affordGeometricSeries(player.rm.realmEnergy[2], base, growth, getBuyableAmount(this.layer, this.id))
                     if (max.gt(this.purchaseLimit().sub(getBuyableAmount(this.layer, this.id)))) { max = this.purchaseLimit().sub(getBuyableAmount(this.layer, this.id)) }
                 let cost = Decimal.sumGeometricSeries(max, base, growth, getBuyableAmount(this.layer, this.id))
                 if (!hasMilestone("s", 16)) player.rm.realmEnergy[2] = player.rm.realmEnergy[2].sub(cost)
